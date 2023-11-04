83d43b5a601b675b6e06291a3ca3b10d0261bdcc
wxoox2000 <wxoox2000@gmail.com>
2023-11-05 00:03:04 +0200

    update3

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,27 +1,27 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-import { SharedLayout } from "./Components/SharedLayout";
-import { Home } from "./pages/Home";
-import { LoginPage } from "./pages/LoginPage";
-import { RestrictedRoute } from "./Components/RestrictedRoute";
+import { SharedLayout } from "./SharedLayout";
+import { Home } from "../pages/Home";
+import { LoginPage } from "../pages/LoginPage";
+import { RestrictedRoute } from "./RestrictedRoute";
 import { useDispatch, useSelector } from "react-redux";
 import {
   selectID,
   selectIsLoggedIn,
   selectIsRefreshing,
-} from "./Redux/auth/selectors";
-import { refreshUser } from "./Redux/auth/operations";
+} from "../Redux/auth/selectors";
+import { refreshUser } from "../Redux/auth/operations";
 import { useEffect } from "react";
-import { fetchFavM, fetchFavS } from "./Redux/account/operations";
-import { PrivateRoute } from "./Components/PrivateRoute";
-import { Favourites } from "./pages/Favourites";
-import { Search } from "./pages/Search";
-import { selectModalData } from "./Redux/selectors";
-import { TrendingPage } from "./pages/TrendingPage";
-import { ComingSoonPage } from "./pages/ComingSoonPage";
-import { Movies } from "./pages/Movies";
-import { Series } from "./pages/Series";
-import { Documentaries } from "./pages/Documentaries";
-import { Icon } from "./pages/Icon";
+import { fetchFavM, fetchFavS } from "../Redux/account/operations";
+import { PrivateRoute } from "./PrivateRoute";
+import { Favourites } from "../pages/Favourites";
+import { Search } from "../pages/Search";
+import { selectModalData } from "../Redux/selectors";
+import { TrendingPage } from "../pages/TrendingPage";
+import { ComingSoonPage } from "../pages/ComingSoonPage";
+import { Movies } from "../pages/Movies";
+import { Series } from "../pages/Series";
+import { Documentaries } from "../pages/Documentaries";
+import { Icon } from "../pages/Icon";
 import { AnimatePresence } from "framer-motion";
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./Components/App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/Watch-movie-web-app/">
           <App />
         </BrowserRouter>
       </PersistGate>
